d198eff7dc833c592f283b8129b33766980c8b3f
martijnkleiweg <mkleiweg@arizona.edu>
2024-04-01 14:30:36 -0700

    Update deploy.sh / requirements.txt

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ pip3 install -r requirements.txt
 
 # Set Flask application and environment variables
 export FLASK_APP=app.py
-export FLASK_ENV=development
+export FLASK_DEBUG=1  # Enable debug mode
 
 # Run the Flask application on port 8080
 flask run --host=0.0.0.0 --port=8080
